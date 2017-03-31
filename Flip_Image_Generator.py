20ba2cc05528e1fb5f5d0cac61e04c1054f11f0f
agsarthak <agarwal.sar@husky.neu.edu>
2017-03-31 15:39:32 -0400

    facial keypoints

---
+++ Flip_Image_Generator.py
@@ -0,0 +1,33 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+from keras.preprocessing.image import ImageDataGenerator
+
+class FlippedImageDataGenerator(ImageDataGenerator):
+    flip_indices = [
+        (0, 2), (1, 3),
+        (4, 8), (5, 9), (6, 10), (7, 11),
+        (12, 16), (13, 17), (14, 18), (15, 19),
+        (22, 24), (23, 25),
+        ]
+
+    def next(self):
+        X_batch, y_batch = super(FlippedImageDataGenerator, self).next()
+        batch_size = X_batch.shape[0]
+        indices = np.random.choice(batch_size, batch_size/2, replace=False)
+        X_batch[indices] = X_batch[indices, :, :, ::-1]
+
+        if y_batch is not None:
+            # Horizontal flip of all x coordinates
+            y_batch[indices, ::2] = y_batch[indices, ::2] * -1
+
+            # Swap places, e.g. left_eye_center_x -> right_eye_center_x
+            for a, b in self.flip_indices:
+                y_batch[indices, a], y_batch[indices, b] = (
+                    y_batch[indices, b], y_batch[indices, a]
+                )
+
+        return X_batch, y_batch
+
